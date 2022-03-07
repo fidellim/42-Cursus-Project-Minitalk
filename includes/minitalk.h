71658c439ba5e744163ea3adb4cd1af43f34c1d6
Fidel Lim <lim.fidel@gmail.com>
2022-03-07 16:07:25 +0400

    add dir for headers

---
+++ includes/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 11:02:41 by flim              #+#    #+#             */
+/*   Updated: 2022/03/07 11:11:41 by flim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <../libft/includes/libft.h>
+
+# define RESET "\033[0m"
+# define BRIGHT_RED "\033[31;1m"
+# define BRIGHT_CYAN "\033[36;1m"
+# define BRIGHT_GREEN "\033[32;1m"
+# define BRIGHT_MAGENTA "\033[35;1m"
+
+#endif
