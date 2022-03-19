ad9ca604e553f1babae994d8f2b1f67f4e327669
Fidel Lim <lim.fidel@gmail.com>
2022-03-19 13:27:16 +0400

    add mandatory files

--- sources/client.c
+++ sources/client.c
@@ -6,30 +6,65 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 15:22:17 by flim              #+#    #+#             */
-/*   Updated: 2022/03/07 11:08:44 by flim             ###   ########.fr       */
+/*   Updated: 2022/03/19 13:24:23 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <../includes/minitalk.h>
+#include "../includes/minitalk.h"
 
-//kill(atoi(argv[1]), SIGINT);
+int	g_str_len;
+
+static void	handle_signal(int sig)
+{
+	static int	count_str;
+
+	if (sig == SIGUSR1)
+		count_str++;
+	if (count_str == g_str_len)
+	{
+		ft_printf(BRIGHT_GREEN "%d byte/s received!" RESET, count_str);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+static void	ft_str_to_binary(int pid, char *str)
+{
+	int	i;
+	int	b;
+
+	i = 0;
+	while (str[i])
+	{
+		b = 7;
+		while (b >= 0)
+		{
+			if ((str[i] & (1 << b)) > 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			b--;
+			usleep(100);
+		}
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int		pid;
-	char	*bits;
 
 	if (argc != 3)
 	{
-		printf("wrong number of arguments\n");
-		return (0);
+		ft_printf(BRIGHT_RED
+			"%s MUST HAVE 3 ARGUMENTS!\n" RESET, argv[0]);
+		exit(EXIT_FAILURE);
 	}
-	pid = atoi(argv[1]);
-	bits = ft_to_bit(argv[2], 0, 0);
-	if (bits == NULL)
-	{
-		printf("allocation went wrong\n");
-		return (0);
-	}
-	ft_send_msg(pid, bits);
-	free(bits);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
+	pid = ft_atoi(argv[1]);
+	g_str_len = ft_strlen(argv[2]);
+	ft_str_to_binary(pid, argv[2]);
+	while (1)
+		pause();
+	return (0);
 }
--- sources/server.c
+++ sources/server.c
@@ -6,40 +6,49 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 15:17:28 by flim              #+#    #+#             */
-/*   Updated: 2022/03/07 11:08:51 by flim             ###   ########.fr       */
+/*   Updated: 2022/03/19 12:43:15 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <../includes/minitalk.h>
+#include "../includes/minitalk.h"
 
-// void	handle_int(int id)
-// {	
-// }
-
-// void	handler(int sig, siginfo_t *info, void *ucontext)
-// {
-// 	printf(BRIGHT_GREEN "THIS HAS BEEN SIGNALLED %d\n", getpid());
-// }
-
-int	main(void)
+static void	ft_binary_to_str(int sig, siginfo_t *info, void *ptr)
 {
-	// struct sigaction	sa;
+	static char	c;
+	static char	num_of_bits;
 
-	// sa.sa_sigaction = ft_confirm;
-	// sa.sa_flags = SA_SIGINFO;
-	//sigaction(SIGINT, &sa, NULL);
-	// sigaction(SIGUSR1, &sa, 0);
-	// sigaction(SIGUSR2, &sa, 0);
-	signal(SIGUSR1, ft_confirm);
-	signal(SIGUSR2, ft_confirm);
-	printf("Process ID: %d\n", getpid());
+	(void) ptr;
+	if (sig == SIGUSR1)
+		c |= 1;
+	if (sig == SIGUSR2)
+		c |= 0;
+	num_of_bits++;
+	if (num_of_bits == 8)
+	{
+		ft_printf("%c", c);
+		num_of_bits = 0;
+		c = 0;
+		kill(info->si_pid, SIGUSR1);
+	}
+	c <<= 1;
+}
+
+int	main(int argc, char **argv)
+{
+	struct sigaction	sa;
+
+	if (argc != 1)
+	{
+		ft_printf(BRIGHT_RED
+			"%s MUST HAVE 1 ARGUMENT!\n" RESET, argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ft_binary_to_str;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf(BRIGHT_GREEN "Server PID: %u\n" RESET, getpid());
 	while (1)
-		;
+		pause();
 	return (0);
-	// while (1 == 1)
-	// {
-	// 	signal(SIGUSR1, ft_confirm);
-	// 	signal(SIGUSR2, ft_confirm);
-	// 	pause();
-	// }
 }
